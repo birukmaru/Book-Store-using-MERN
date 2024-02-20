58ecb04cd6182821cfdedefe451591e66706e94f
Brak yonie <birukmaru12@gmail.com>
2024-02-20 21:36:15 +0300

    Add final work

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
-export const PORT = 5550;
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://birukyoni12:BrakMaru12@books-store-mern.mjlzrel.mongodb.net/books-collection?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,34 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+// Middleware for parsing request body
+
+app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
